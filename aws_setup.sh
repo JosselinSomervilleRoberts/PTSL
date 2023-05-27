b9d8754dc3460ada2f5b4bdfcbe969e537ebda65
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-05-27 01:38:30 -0700

    Fix aws_setup

--- aws_setup.sh
+++ aws_setup.sh
@@ -17,20 +17,22 @@ rm -r -f mujoco210-linux-x86_64.tar.gz
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ubuntu/.mujoco/mujoco200/bin' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/nvidia' >> ~/.bashrc
 source ~/.bashrc
-conda activate rlgarage
-yes y | sudo apt-get install patchelf
-yes y | sudo apt-get install libglew-dev
-yes y | pip install mujoco
-yes y | pip install scipy
-yes y | sudo apt install libosmesa6-dev libgl1-mesa-glx libglfw3
 cd $pwd
-
 
 # Create conda env and activate it
 conda env create -f environment.yml
 conda init bash
 source ~/.bashrc
 conda activate mtrl
+
+# Finish mujoco installs
+yes y | sudo apt-get install patchelf
+yes y | sudo apt-get install libglew-dev
+yes y | pip install mujoco
+yes y | pip install scipy
+yes y | sudo apt install libosmesa6-dev libgl1-mesa-glx libglfw3
+
+# Additional installs (metaworld, mtenv)
 pip install git+https://github.com/Farama-Foundation/Metaworld.git@af8417bfc82a3e249b4b02156518d775f29eb28 -y
 pip install "mtenv[metaworld]" -y
 pip install git+https://github.com/JosselinSomervilleRoberts/JossPythonToolbox.git -y
