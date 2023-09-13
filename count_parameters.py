998af2b75429b97c9fe64a94994b982f5da6ad9a
Josselin Somerville Roberts <josselin.somerville@gmail.com>
2023-09-13 14:51:25 -0700

    Add options to count parameters

--- count_parameters.py
+++ count_parameters.py
@@ -17,6 +17,15 @@ def parse_args():
     parser.add_argument(
         "--num_envs", type=int, default=10, help="Number of environments."
     )
+    parser.add_argument(
+        "--input_dim", type=int, default=100, help="Input dimension."
+    )
+    parser.add_argument(
+        "--no_initial_project_down", action="store_true", help="No initial projection down."
+    )
+    parser.add_argument(
+        "--no_encoder", action="store_true", help="No encoder."
+    )
     args = parser.parse_args()
     return args
 
@@ -27,12 +36,15 @@ def main(args):
     pal_dim = args.pal_dim
     num_envs = args.num_envs
 
-    num_params = 18104 * 3  # There are 3 Encoders
+    num_params = 0
+    if not args.no_encoder:
+        num_params += 18104 * 3  # There are 3 Encoders
 
     # PAL
-    for output_dim, input_dim in zip([8, 1, 1, 1, 1], [100, 104, 104, 104, 104]):
+    for output_dim, input_dim in zip([8, 1, 1, 1, 1], [0, 4, 4, 4, 4]):
+        input_dim += args.input_dim
         pal_params = 0
-        pal_params = (num_layers) * (
+        pal_params = (num_layers - args.no_initial_project_down) * (
             pal_dim**2 * num_envs + pal_dim * num_envs
         )  # Set of individual layers
         pal_params += (
@@ -40,11 +52,16 @@ def main(args):
         )  # Last individual layer
         pal_params += (num_layers - 1) * (
             hidden_dim**2 + hidden_dim
-        )  # Shared layers (middles
+        )  # Shared layers (middles)
         pal_params += input_dim * hidden_dim + hidden_dim  # First shared layer
         pal_params += hidden_dim * output_dim + output_dim  # Last shared layer
         # Projections
-        pal_params += input_dim * pal_dim  # First projection
+        if not args.no_initial_project_down:
+            print("Initial projection down")
+            pal_params += input_dim * pal_dim  # First projection
+        else:
+            print("No initial projection down")
+            pal_params += input_dim * pal_dim * num_envs + pal_dim * num_envs # First individual layer
         pal_params += 2 * pal_dim * hidden_dim  # Middle projections
         print("PAL parameters: ", pal_params)
 
